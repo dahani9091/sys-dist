7d5fe386d634796cfa215746798081b506923d70
Adel Dahani <adeldahani@gmail.com>
2022-03-28 22:59:10 +0000

    l9wada f tiran 2

---
+++ multicast-udp/mulicast_receiver_test.java
@@ -0,0 +1,49 @@
+package communication.multicast;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class mulicast_receiver_test {
+    
+    public static void main(String[] args) {
+        try{
+            InetAddress group = InetAddress.getByName("255.0.0.1");
+
+            MulticastSocket mcs = new MulticastSocket(8888);
+    
+            mcs.joinGroup(group);
+    
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            while(true){
+    
+                DatagramPacket p = new DatagramPacket(receiveData,receiveData.length); 
+                
+                mcs.receive(p);
+    
+                String msg = new String(p.getData(),0,p.getLength());
+    
+                System.out.println(msg);
+
+                sendData = (new String("msg from the receiver ")).getBytes();
+
+                p.setData(sendData);
+                p.setLength(sendData.length);
+
+                mcs.send(p);
+    
+            }
+    
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+
+    }
+    
+
+
+
+
+}
---
+++ multicast-udp/multicast_sender_test.java
@@ -0,0 +1,46 @@
+package communication.multicast;
+
+import java.net.MulticastSocket;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class multicast_sender_test {
+
+    public static void main(String[] args) {
+        try{
+            InetAddress group = InetAddress.getByName("255.0.0.1");
+            MulticastSocket mcs = new MulticastSocket(8888);
+            mcs.joinGroup(group);
+
+            while(true){
+                System.out.println("Please kteb chi 9lwa: ");
+                String msg = (new Scanner(System.in)).nextLine();
+                byte[] sendData = msg.getBytes();
+                DatagramPacket p = new DatagramPacket(sendData,sendData.length,group,8888);
+
+                mcs.send(p);
+
+                // receive
+                byte[] receiveData = new byte[1024];
+
+                DatagramPacket p2 = new DatagramPacket(receiveData,receiveData.length);
+                
+                mcs.receive(p2);
+
+                String received_msg = new String(receiveData,0,receiveData.length);
+
+                System.out.println(received_msg);
+
+
+            }
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+
+    }
+    
+}

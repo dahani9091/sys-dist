dd676a1a14171fa2c7e89ea42d3975b88bde1700
Adel Dahani <adeldahani@gmail.com>
2022-03-29 01:07:01 +0000

    HPC

--- HPC/omp.cpp
+++ HPC/omp.cpp
@@ -1,20 +1,37 @@
 #include <stdio.h>
-#ifdef _OPENMP
 #include<omp.h>
-#endif
 int main(int argc, char* argv[ ])
 {
-    #ifdef _OPENMP
     int iam;
-    #pragma omp parallel n
-    private(iam) /* the parallel block starts here */
+    omp_set_num_threads(5);
+    #pragma omp parallel 
+    
     {
         iam=omp_get_thread_num();
+        nd = omp_get_num_threads();
+        
+
         #pragma omp critical
         printf("Hello from %d\n",iam);
     } /* the parallel block ends here */
-    #else
-        printf("Hello, this is a serial program.\n");
-    #endif
+    //printf("Hello, this is a serial program.\n");
     return 0;
 }
+
+
+/// loops
+
+
+...
+double time1 = omp_get_wtime();
+sum = 0.0;
+dx = 1.0 / (double) intervals;
+#pragma omp parallel for private(x,f) reduction(+:sum)
+for (i = 1; i <= n; i++) {
+    x = dx * ((double) (i - 0.5));
+    f = 4.0 / (1.0 + x*x);
+    sum = sum + f;
+}
+pi = dx*sum;
+time2 = omp_get_wtime() - time1;
+...
